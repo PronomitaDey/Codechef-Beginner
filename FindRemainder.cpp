cccbfeff0b0c6fdfb4b88ac69c763a3434412ddc
PronomitaDey <dey.pronomita@gmail.com>
2017-09-23 04:46:40 +0530

    Add files via upload

---
+++ FindRemainder.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int t;
+  cin>>t;
+  int arr[2][t];
+  int i=0,j=0;
+  for(i=0;i<t;i++){
+		  cin>>arr[i][0]>>arr[i][1];
+  }
+  for(j=0;j<t;j++){
+	  cout<<arr[j][0]/arr[j][1];
+  }
+	 return 0;
+}
+  
